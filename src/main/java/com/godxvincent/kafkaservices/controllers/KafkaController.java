b56e452ddce084a1407955e966ce9a7ad9008b94
Zarakick <kmarzola21@gmail.com>
2019-12-13 17:38:11 -0500

    Realizacion de los metodos rest

--- src/main/java/com/godxvincent/kafkaservices/controllers/KafkaController.java
+++ src/main/java/com/godxvincent/kafkaservices/controllers/KafkaController.java
@@ -1,12 +1,11 @@
 package com.godxvincent.kafkaservices.controllers;
 
 import com.godxvincent.kafkaservices.db2connection.Db2connection;
+import com.godxvincent.kafkaservices.models.Emisor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.godxvincent.kafkaservices.producers.Producer;
 
@@ -23,10 +22,32 @@ public class KafkaController {
         this.producer = producer;
     }
 
-    @PostMapping(value = "/publish")
-    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
-        ResponseEntity<String>  result = db2connectionTest.db2connection();
-        this.producer.sendMessage(message);
+    @PostMapping(value = "/create")
+    public Emisor crearEmisor(@RequestBody Emisor emisor) {
+        ResponseEntity<Emisor> emisorCreado = db2connectionTest.createEntity(emisor);
+        this.producer.sendMessage(emisor.toString());
+        return emisorCreado.getBody();
     }
-    
+
+    @GetMapping(value = "/select")
+    public Emisor consultarEmisor(@RequestParam String idEmisor) {
+        ResponseEntity<Emisor> emisorCreado = db2connectionTest.selectEntity(idEmisor);
+        this.producer.sendMessage(idEmisor);
+        return emisorCreado.getBody();
+    }
+
+    @PutMapping(value = "/update")
+    public Emisor actualizarEmisor(@RequestBody Emisor emisor) {
+        ResponseEntity<Emisor> emisorCreado = db2connectionTest.updateEntity(emisor);
+        this.producer.sendMessage(emisorCreado.getBody().toString());
+        return emisorCreado.getBody();
+    }
+
+    @DeleteMapping(value = "/delete")
+    public String eliminarEmisor(@RequestParam String idEmisor) {
+        ResponseEntity<String> emisorEliminado = db2connectionTest.deleteEntity(idEmisor);
+        this.producer.sendMessage(emisorEliminado.toString());
+        return emisorEliminado.toString();
+    }
+
 }
--- src/main/java/com/godxvincent/kafkaservices/db2connection/Db2connection.java
+++ src/main/java/com/godxvincent/kafkaservices/db2connection/Db2connection.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,13 +18,50 @@ public class Db2connection {
     JdbcTemplate jdbcTemplate;
 
     //TODO Ajustar metodos de consulta
-    public ResponseEntity<String> db2connection() {
+
+    public ResponseEntity<Emisor> selectEntity(String idEmisor) {
+        List<String> list = new ArrayList();
+        Emisor emisorConsultado = new Emisor();
+        jdbcTemplate.query(
+                "SELECT * FROM EMISORES", new Object[]{},
+                (rs, rowNum) -> new Emisor(rs.getLong("id"),
+                        rs.getTimestamp("name")
+                ))
+                .forEach(thing -> list.add(thing.toString()));
+        return new ResponseEntity<Emisor>(emisorConsultado, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Emisor> createEntity(Emisor emisor) {
+
+        String sqlInsertEmistor = "INSERT INTO EMISOR " +
+                "(EMISORES_ID, EMISORES_ISIN, EMISORES_DOC, ) " +
+                "VALUES " +
+                "(:emisoresId, :emisoresIsin, :emisoresDoc)";
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(emisor);
+        //jdbcTemplate.queryForObject();
+
+        return new ResponseEntity<Emisor>(emisor, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Emisor> updateEntity(Emisor emisor) {
         List<String> list = new ArrayList();
         list.add("Table data...");
         jdbcTemplate.query(
                 "SELECT * FROM EMISORES", new Object[]{},
                 (rs, rowNum) -> new Emisor(rs.getLong("id"),
-                                           rs.getTimestamp("name")
+                        rs.getTimestamp("name")
+                ))
+                .forEach(thing -> list.add(thing.toString()));
+        return new ResponseEntity<>(emisor, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> deleteEntity(String idEmisor) {
+        List<String> list = new ArrayList();
+        list.add("Table data...");
+        jdbcTemplate.query(
+                "SELECT * FROM EMISORES", new Object[]{},
+                (rs, rowNum) -> new Emisor(rs.getLong("id"),
+                        rs.getTimestamp("name")
                 ))
                 .forEach(thing -> list.add(thing.toString()));
         return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
